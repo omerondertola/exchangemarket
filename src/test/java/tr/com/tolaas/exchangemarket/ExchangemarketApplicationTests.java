fbd7efbb7c35ed728a29c7359386daa879ef398d
omerondertola <omerondertola@gmail.com>
2021-05-31 22:42:06 +0300

    Initial import

---
+++ src/test/java/tr/com/tolaas/exchangemarket/ExchangemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.tolaas.exchangemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangemarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
