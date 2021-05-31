e375d4bb13fc484b00ca1efd0a8da5870b775ace
omerondertola <omerondertola@gmail.com>
2021-05-31 22:44:57 +0300

    Initial import

---
+++ src/main/java/tr/com/tolaas/exchangemarket/ExchangemarketApplication.java
@@ -0,0 +1,13 @@
+package tr.com.tolaas.exchangemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangemarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangemarketApplication.class, args);
+    }
+
+}
